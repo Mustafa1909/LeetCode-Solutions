825cda446f554507544083f9f919940f384b1d8c
Mustafa <yousufmusf@gmail.com>
2026-05-25 22:07:27 +0530

    Kaden's Algorithm added

---
+++ Kadnes'Algorithm/918_Maximum_Sum_Circular_Subarray.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxSubarraySumCircular(int[] nums) {
+        int currmax = nums[0];
+        int maxsum = nums[0];
+        int currmin=nums[0];
+        int minsum =nums[0];
+        int total = nums[0];
+        for(int i=1;i<nums.length;i++){
+            currmax=Math.max(nums[i],currmax+nums[i]);
+            maxsum=Math.max(currmax,maxsum);
+
+            currmin=Math.min(nums[i],currmin+nums[i]);
+            minsum=Math.min(currmin,minsum);
+
+            total+=nums[i];
+            
+        }
+        if(maxsum<0){
+            return maxsum;
+        }
+        return Math.max(maxsum,total-minsum);
+    }
+}

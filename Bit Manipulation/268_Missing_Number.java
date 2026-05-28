3367459e49b6ce0f367d1a1f9c10ab9e3dce4ad4
Mustafa <yousufmusf@gmail.com>
2026-05-28 21:26:50 +0530

    Bit Manipulation

---
+++ Bit Manipulation/268_Missing_Number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int xor = 0;
+ for(int i=0;i<=n;i++){
+            xor = xor ^ i;
+        }
+        for(int i=0;i<nums.length;i++){
+            xor = xor ^ nums[i];
+        }
+
+       
+       return xor;
+
+
+    }
+}
